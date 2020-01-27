3d3f5553221a627703570b21fb654a2710d7181e
mitsunori-morita <mitsunori.morita@speee.jp>
2020-01-27 12:59:30 +0900

    form comp

--- _js/app.js
+++ _js/app.js
@@ -1,13 +1,13 @@
 // 読み込み時
-$(function() {
-  var fadeAdd = function(element, className) {
+$(function () {
+  var fadeAdd = function (element, className) {
     $(element).addClass(className);
   };
 
-  var fadeItem1 = function() {
+  var fadeItem1 = function () {
     fadeAdd(".mainVisual__Lead-title", "in-visible");
   };
-  var fadeItem2 = function() {
+  var fadeItem2 = function () {
     fadeAdd(".mainVisual__Lead-sub", "in-visible");
   };
 
@@ -15,7 +15,7 @@ $(function() {
   var $pageTaget = $(".mainVisual__Lead");
   if (!$pageTaget.length) return false;
   var count = 0;
-  var countup = function() {
+  var countup = function () {
     // console.log(count++);
     count++;
     var id = setTimeout(countup, 1000);
@@ -29,22 +29,22 @@ $(function() {
     }
   };
 
-  window.onload = function() {
+  window.onload = function () {
     setTimeout(fadeItem1, 2000);
     setTimeout(fadeItem2, 3000);
     countup();
   };
 });
 
-$(function() {
+$(function () {
   //リクルートモーダル　職種募集部分
-  $(".recruit__recruitingList-item").on("click", function() {
+  $(".recruit__recruitingList-item").on("click", function () {
     var index = $(this).index() + 1;
     $(".recruit__modalList").fadeIn();
     $(".recruit__modalList li:nth-child(" + index + ")").fadeToggle(200);
   });
 
-  $(".recruit__modalList-Close").on("click", function() {
+  $(".recruit__modalList-Close").on("click", function () {
     $(".recruit__modalList li").hide();
     $(this)
       .parents()
@@ -53,9 +53,9 @@ $(function() {
   });
 });
 
-$(function() {
+$(function () {
   //footer
-  $(".footer__menuBox-title").on("click", function() {
+  $(".footer__menuBox-title").on("click", function () {
     $(this)
       .next()
       .slideToggle();
@@ -63,7 +63,7 @@ $(function() {
   });
 
   //sp nav menu
-  $(".navSp__Button").on("click", function() {
+  $(".navSp__Button").on("click", function () {
     $(this).toggleClass("on");
     $(this)
       .prev()
@@ -74,7 +74,7 @@ $(function() {
   if (!$pageTop.length) return false;
 
   $pageTop.hide();
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     if ($(this).scrollTop() > 1500) {
       $pageTop.fadeIn();
     } else {
@@ -82,7 +82,7 @@ $(function() {
     }
   });
 
-  $('a[href^="#"]').click(function() {
+  $('a[href^="#"]').click(function () {
     var href = $(this).attr("href");
     var target = $(href == "#" || href == "" ? "html" : href);
     var position = target.offset().top;
@@ -97,16 +97,16 @@ $(function() {
   });
 });
 
-$(function() {
+$(function () {
   //aboutページ
-  var fadeAdd = function(element, className) {
+  var fadeAdd = function (element, className) {
     $(element).addClass(className);
   };
 
-  var fadeItem1 = function() {
+  var fadeItem1 = function () {
     fadeAdd(".aboutPage__mainvisual-title", "in-visible");
   };
-  var fadeItem2 = function() {
+  var fadeItem2 = function () {
     fadeAdd(".aboutPage__mainvisual-text", "in-visible");
   };
 
@@ -116,4 +116,15 @@ $(function() {
 
   setTimeout(fadeItem1, 2000);
   setTimeout(fadeItem2, 3000);
+});
+
+$(function () {
+  //  form modal
+  $(".privacy-modal-Button").on("click", function () {
+    $(".privacy-modal-contents").fadeIn();
+  });
+
+  $(".privacy-modal-close-Button").on("click", function () {
+    $(".privacy-modal-contents").fadeOut();
+  });
 });
