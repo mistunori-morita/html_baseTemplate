030827588a2e1ef4f618edf068caa830772f0cfd
mitsunori-morita <mitsunori.morita@speee.jp>
2020-02-10 14:03:56 +0900

    header scroll時の変更、scriptの削除

--- _js/app.js
+++ _js/app.js
@@ -10,30 +10,24 @@ $(function () {
   var fadeItem2 = function () {
     fadeAdd(".mainVisual__Lead-sub", "in-visible");
   };
-
-  //文字色変更 mv
-  var $pageTaget = $(".mainVisual__Lead");
-  if (!$pageTaget.length) return false;
-  var count = 0;
-  var countup = function () {
-    // console.log(count++);
-    count++;
-    var id = setTimeout(countup, 1000);
-
-    if (count == 8) {
-      $pageTaget.children().css("color", "#fff");
-    } else if (count > 13) {
-      $pageTaget.children().css("color", "#5576ff");
-
-      count = 0;
-    }
-  };
-
   window.onload = function () {
     setTimeout(fadeItem1, 2000);
     setTimeout(fadeItem2, 3000);
-    countup();
   };
+});
+
+$(function(){
+  //headerスクロールメニュー
+  var $header = $(".header");
+  if (!$header.length) return false;
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 800) {
+      $header.addClass('on');
+    } else {
+      $header.removeClass('on');
+    }
+  });
 });
 
 $(function () {
